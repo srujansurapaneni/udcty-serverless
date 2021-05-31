1391fa1b1410f63ae0b9d8b3e9d7dc4a09d4dc6c
srujan surapaneni <srujansurapaneni@hotmail.com>
2021-05-30 22:15:16 -0400

    Delete Ex 2 directory

--- Ex 2/greet_lambda.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def lambda_handler(event, context):
-    resp = "{} from Lambda!".format(os.environ['greeting'])
-    print(resp)
-    return resp
--- Ex 2/variables.tf
+++
@@ -1,5 +0,0 @@
-# TODO: Define the variable for aws_region
-variable "greeting" {
-  type    = string
-  default = "Hey Folks !"
-}
